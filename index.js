864b8a5e5430e29adfd984122455a403dae0698e
Agnieszka Brown <abrown2012@icloud.com>
2021-05-27 14:03:03 -0400

    commit tree 8f0703b8c3b3d72dc5ca09968493bf9c40cc8b0f

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
   // Write your code here!
+  if (length <= 400){
+    return 'This one is on me!'
+  } else if (length > 2000 && length <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city ==='NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipType){
+  if (tipType === 'generous') {
+    return 'Thank you so much.'
+  } else if (tipType === 'not as generous'){
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
   // Write your code here!
 }
